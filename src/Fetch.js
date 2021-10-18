3dadf07dc813d7b9d587989c2d7d70c53a5c9a9c
manvisharma2130 <76566032+manvisharma2130@users.noreply.github.com>
2021-10-18 22:53:24 +0530

    Update Fetch.js

--- src/Fetch.js
+++ src/Fetch.js
@@ -49,145 +49,98 @@ export default class Fetch extends Component {
 
         return (
 
-            <
-            div >
+            <div>
 
-            <
-            div id = "overlay" >
-            <
-            img src = "./img/preloader.gif"
-            alt = "Preloader icon" / >
-            <
-            /div>
+            <div id = "overlay">
+            <img src = "./img/preloader.gif" alt = "Preloader icon" / >
+            </div>
 
-            <
-            main >
+            <main>
 
-            <
-            div id = "table-section" >
+            <div id = "table-section">
 
-            <
-            form action = "/" >
-            <
-            img src = 'https://raw.githubusercontent.com/qaifikhan/Assignment-9-Basic-Admin-Panel/a4f98fc05db6d11c1dff0e84d9f0fd67f8927e0a/img/search-icon.svg'
-            alt = "Search Icon" / >
-            <
-            input onChange = { this.handleChange }
-            type = "text"
-            placeholder = "Enter something"
-            id = "search-box" / >
-            <
-            /form>
+            <form action = "/">
+            <img src = 'https://raw.githubusercontent.com/qaifikhan/Assignment-9-Basic-Admin-Panel/a4f98fc05db6d11c1dff0e84d9f0fd67f8927e0a/img/search-icon.svg'
+            alt = "Search Icon"/>
+            <input onChange = { this.handleChange } type = "text" placeholder = "Enter something" id = "search-box"/>
+            </form>
 
-            <
-            div id = "table-wrapper" >
+            <div id = "table-wrapper" >
 
-            <
-            div id = "table-headers" >
-            <
-            table >
-            <
-            thead >
+            <div id = "table-headers">
+            <table>
+            <thead>
+            
+            <tr>
+            <th className = "column1" > Id </th> 
+            <th className = "column2" > FirstName </th> 
+            <th className = "column3" > LastName </th> 
+            <th className = "column4" > Email </th> 
+            
+            <th className = "column5" > Phone </th> 
+            </tr> 
+            </thead> 
+            </table> 
+            </div>
 
-            <
-            tr >
-            <
-            th className = "column1" > Id < /th>  <
-            th className = "column2" > FirstName < /th>  <
-            th className = "column3" > LastName < /th>  <
-            th className = "column4" > Email < /th> 
-
-            <
-            th className = "column5" > Phone < /th>  <
-            /tr>  <
-            /thead>  <
-            /table>  <
-            /div>
-
-            <
-            div id = "table-data" >
-            <
-            table >
-            <
-            tbody >
+            <div id = "table-data">
+            <table>
+            <tbody>
 
 
             {
                 this.state.filteredData.map((item) =>
 
-                    <
-                    tr onClick = {
-                        () => this.handleClick(item.id) }
-                    key = { item.id }
+                    <tr onClick = {() => this.handleClick(item.id) } key = { item.id }
                     className = "data-row" >
-                    <
-                    td className = "column1" > { item.id } < /td>  <
-                    td className = "column2" > { item.firstName } < /td>  <
-                    td className = "column3" > { item.lastName } < /td>  <
-                    td className = "column4" > { item.email } < /td>  <
-                    td className = "column5" > { item.phone } < /td>  <
-                    /tr>
+                    <td className = "column1"> { item.id } </td> 
+                    <td className = "column2"> { item.firstName } </td> 
+                    <td className = "column3"> { item.lastName } </td> 
+                    <td className = "column4"> { item.email } </td> 
+                    <td className = "column5"> { item.phone } </td> 
+                    </tr>
                 )
             }
 
 
 
-            <
-            /tbody>  <
-            /table>  <
-            /div>
+            </tbody> 
+            </table> 
+            </div>
 
-            <
-            /div>
+            </div>
 
-            <
-            /div>
+            </div>
 
 
 
 
-            <
-            div id = "info-wrapper" >
-            <
-            h1 > Details < /h1>  <
-            p > Click on a table item to get detailed information < /p> {
-            this.state.admin.filter(re => re.id == this.state.id).map(item =>
+            <div id = "info-wrapper">
+            <h1> Details </h1> 
+            <p> Click on a table item to get detailed information </p> {
+                this.state.admin.filter(re => re.id == this.state.id).map(item =>
 
-                <
-                div id = "info-content"
-                style = {
-                    { display: `${this.state.change}` }
-                } >
-                <
-                div > < b > User selected: < /b> {item.firstName} {item.lastName} </div >
-                <
-                div >
-                <
-                b > Description: < /b>  <
-                div id = "para" > { item.description } <
-                /div> <
-                /div>  <
-                div className = "address" > < b > Address: < /b> {item.address.streetAddress}</div >
-                <
-                div className = "address" > < b > City: < /b> {item.address.city}</div >
-                <
-                div className = "address" > < b > State: < /b> {item.address.state}</div >
-                <
-                div className = "address" > < b > Zip: < /b> {item.address.zip}</div >
-                <
-                /div>
-            )
-        }
+                    <div id = "info-content" style = {{ display: `${this.state.change}` } } >
+                    <div> <b> User selected: </b> {item.firstName} {item.lastName} </div>
+                    <div>
+                    <b> Description: </b> 
+                    <div id = "para" > { item.description } 
+                    </div>
+                     </div> 
+                     <div className = "address" > < b > Address: </b> {item.address.streetAddress}</div >
+                    <div className = "address" > < b > City: </b> {item.address.city}</div >
+                    <div className = "address" > < b > State: </b> {item.address.state}</div >
+                    <div className = "address" > < b > Zip: </b> {item.address.zip}</div >
+                    </div>
+                )
+            }
 
-        <
-        /div>
+            </div>
 
-        <
-        /main>
+            </main>
 
-        <
-        /div>
+            </div>
 
-    )
-}
+        )
+    }
 }
